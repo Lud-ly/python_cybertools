dcd43b1a922155359b6fde866d61270aea4dc588
Ludovic Mouly <ludom82@gmail.com>
2025-12-19 18:50:13 +0100

    prod

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    # En dev : port 5050
-    # En prod : port fourni par l'hébergeur
+    # Port dynamique pour Render
     port = int(os.environ.get("PORT", 5050))
-    app.run(debug=False, host="0.0.0.0", port=port)
+    # Debug False en production
+    debug = os.environ.get("FLASK_ENV") != "production"
+    app.run(debug=debug, host="0.0.0.0", port=port)
